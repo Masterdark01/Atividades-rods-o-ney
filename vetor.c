8d418535eb526d8062a50906901fb3cdd345a1f0
Nickolas Sanches <nickolassanches01@gmail.com>
2024-10-07 21:55:04 -0300

    Create vetor.c

---
+++ vetor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include <windows.h>
+
+int main(){
+    
+    int vetor [10];
+    int i;
+
+    for (i = 0; i < 10; i++){
+        system("cls");
+        printf("digite um valor: ");
+        scanf("%d", &vetor[i]);
+    }
+
+    for(i = 0; i < 10; i++){
+        printf("vet[%d] = %d\n", i , vetor[i]);
+    }
+
+    return 0;
+}
